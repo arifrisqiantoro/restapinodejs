7c31e294fddce1225b9cd0502e6e5d17a4154667
unknown <arifrisqiantoro@gmail.com>
2022-07-22 11:09:33 +0700

    menambahkan response data msuser byid

--- routes.js
+++ routes.js
@@ -1,14 +1,14 @@
 "use strict";
 
 module.exports = function (app) {
-  let viewjson = require("./controller");
+  let jsonresponse = require("./controller");
 
-  app.route("/").get(viewjson.index);
+  app.route("/").get(jsonresponse.index);
 
   //menampilkan semua data ms_user
-  app.route("/tampildatauser").get(viewjson.tampildatauser);
+  app.route("/tampildatauser").get(jsonresponse.tampildatauser);
 
   //menampilkan data ms_user berdasarkan id_user
 
-  app.route("/tampildatauser/:id").get(viewjson.tampildatauserbyid);
+  app.route("/tampildatauser/:id").get(jsonresponse.tampildatauserbyid);
 };
